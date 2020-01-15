6f5b39915e76b62521f042fcac9e3b80b27b69c3
‘Linda <linda.lindgr3n@hotmail.com>
2020-01-15 16:54:02 +0100

    klient and chatbot is now able to connect to the server

--- ApiProject/ApiProject/Program.cs
+++ ApiProject/ApiProject/Program.cs
@@ -32,26 +32,119 @@ namespace ApiProject
 
         static async Task Main()
         {
-            while(true)
+            // start connection
+            string serverIP = "192.168.156.54";
+            int port = 8080;
+
+            try
             {
-                Thread.Sleep(5000);
+                // Connect to server
+                TcpClient client = new TcpClient(serverIP, port);
+                Console.WriteLine("Connected to server");
+
+                // create stream
+                NetworkStream stream = client.GetStream();
+
+                // start 2 threads, stream write & stream read
+                // Creating and initializing thread 
+                Thread ListenThr = new Thread(() => ListenThread(client, stream));
+                Task SendThr = new Task(async()  => await SendThread(serverIP, client, stream));
+
+                ListenThr.Start();
+                SendThr.Start();
+
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not connect");
+            }
+        }
+
+        // listen for messages
+        static void ListenThread(TcpClient client, NetworkStream stream)
+        {
+            // NetworkStream stream = client.GetStream();
+            while (true)
+            {
+                if (stream.DataAvailable)
+                {
+                    byte[] recievedBuffer = new byte[400];
+
+                    try
+                    {
+                        stream.Read(recievedBuffer, 0, recievedBuffer.Length);
+                    }
+                    catch (Exception)
+                    {
+                        Console.WriteLine("Stream could not be read..");
+                    }
+
+                    StringBuilder msg = new StringBuilder();
+
+                    foreach (byte b in recievedBuffer)
+                    {
+                        if (b.Equals(0))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            msg.Append(Convert.ToChar(b).ToString());
+                        }
+                    }
+
+                    // Blue color from other users, white when I write
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                    string message = msg.ToString();
+                    Console.WriteLine("\n" + message);
+                    Console.ForegroundColor = ConsoleColor.White;
+
+                    // flush stream when done
+                    stream.Flush();
+                }
+            }
+        }
+
+        // send messages to server
+        static async Task SendThread(string serverIP, TcpClient client, NetworkStream stream)
+        {
+            while (true)
+            {
+                Thread.Sleep(100000);
 
                 // create protocolobject
                 CreateProtocol protocolObject = new CreateProtocol();
 
                 // Fill protocol with data
                 createProtocol(protocolObject);
+                protocolObject.recipientIP = serverIP;
 
-                //// get data from api
-                //await getDataFromApi(protocolObject);
+                // get data from api
+                await getDataFromApi(protocolObject);
 
                 // make protocol object to JSON
                 string protocol = JsonConvert.SerializeObject(protocolObject);
 
-               // Console.WriteLine(protocol);
+                Console.Write("'chatbot' says > ");
+                Console.WriteLine(protocol);
 
-                // send message
-                MessageThread(protocol);
+                try
+                {
+                    int byteCount = Encoding.ASCII.GetByteCount(protocol);
+                    byte[] sendDataa = new byte[byteCount];
+                    sendDataa = Encoding.ASCII.GetBytes(protocol);
+
+                    // write message to stream
+                    stream.Write(sendDataa, 0, sendDataa.Length);
+
+                    // flush when done
+                    stream.Flush();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Message could not be sent..");
+                }
             }
         }
 
@@ -119,50 +212,52 @@ namespace ApiProject
             // create json
             protocolObject.date = S_DateTime;
             protocolObject.senderIP = myIP;
-            protocolObject.nickname = "Chatbot";
-            protocolObject.recipientIP = "172.16.117.80";
+            protocolObject.nickname = "Chatbot";    
 
         }
 
-        // send message
-        static void MessageThread(string protocol)
-        {
-            string serverIP = "172.16.117.80";
-            int port = 8080;
+        //// send message
+        //static void MessageThread(string protocol)
+        //{
+        //    string serverIP = "172.16.117.80";
+        //    int port = 8080;
 
-            try
-            {
-                // SEND DATA
-                TcpClient clienta = new TcpClient(serverIP, port);
-                int byteCount = Encoding.ASCII.GetByteCount(protocol);
+        //    try
+        //    {
+        //        // SEND DATA
+        //        TcpClient clienta = new TcpClient(serverIP, port);
+        //        int byteCount = Encoding.ASCII.GetByteCount(protocol);
 
-                byte[] sendDataa = new byte[byteCount];
+        //        byte[] sendDataa = new byte[byteCount];
 
-                sendDataa = Encoding.ASCII.GetBytes(protocol);
-                NetworkStream stream = clienta.GetStream();
-                stream.Write(sendDataa, 0, sendDataa.Length);
+        //        sendDataa = Encoding.ASCII.GetBytes(protocol);
+        //        NetworkStream stream = clienta.GetStream();
+        //        stream.Write(sendDataa, 0, sendDataa.Length);
 
-                // RESPONSE
-                // Buffer to store the response bytes.
-                sendDataa = new Byte[256];
+        //        // RESPONSE
+        //        // Buffer to store the response bytes.
+        //        sendDataa = new Byte[256];
 
-                // String to store the response ASCII representation.
-                String responseData = String.Empty;
+        //        // String to store the response ASCII representation.
+        //        String responseData = String.Empty;
 
-                // Read the first batch of the TcpServer response bytes.
-                Int32 bytes = stream.Read(sendDataa, 0, sendDataa.Length);
-                responseData = System.Text.Encoding.ASCII.GetString(sendDataa, 0, bytes);
-                Console.WriteLine(responseData);
+        //        // Read the first batch of the TcpServer response bytes.
+        //        Int32 bytes = stream.Read(sendDataa, 0, sendDataa.Length);
+        //        responseData = System.Text.Encoding.ASCII.GetString(sendDataa, 0, bytes);
+        //        Console.WriteLine(responseData);
 
-                // stream.Close();
-                // clienta.Close();
-                stream.Flush();
+        //        // stream.Close();
+        //        // clienta.Close();
+        //        stream.Flush();
 
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Message could not be sent..");
-            }
-        }
+        //    }
+        //    catch (Exception)
+        //    {
+        //        Console.WriteLine("Message could not be sent..");
+        //    }
+        //}
+
+        // listen for incoming messages
+
     }
 }
